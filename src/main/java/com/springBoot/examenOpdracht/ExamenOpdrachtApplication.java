8683e2b07dbd57cfd76e0050ca475091fd79af4b
ynoynaert <youna.noynaert@telenet.be>
2024-04-23 15:54:04 +0200

    project aangemaakt

---
+++ src/main/java/com/springBoot/examenOpdracht/ExamenOpdrachtApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.examenOpdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenOpdrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenOpdrachtApplication.class, args);
+	}
+
+}
