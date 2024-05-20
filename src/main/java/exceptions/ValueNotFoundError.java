b7306805c37bb4115a163e6c0785c17ee7fd903b
ynoynaert <youna.noynaert@student.hogent.be>
2024-05-20 10:29:21 +0200

    klaar!

--- src/main/java/exceptions/ValueNotFoundError.java
+++ src/main/java/exceptions/ValueNotFoundError.java
@@ -1,10 +1,16 @@
 package exceptions;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class ValueNotFoundError extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
+	
+	private static final ResourceBundle messages = ResourceBundle.getBundle("i18n/messages", Locale.getDefault());
+
 
 	public ValueNotFoundError(String c, Long id) {
-		super(String.format("%s with ID %s not found", c, id));
+		super(String.format(messages.getString("valueNotFoundError"), new Object[] {c, id}));
 	}
 }
